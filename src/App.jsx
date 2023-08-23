d8522c61af6f68508c0bd936e49997efe172dda7
Gabegar <gagarcia190@gmail.com>
2023-08-23 16:14:42 -0400

    updates to mobile design - pre desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,98 @@
+import { useState } from "react";
+
 import StarIcon from "./components/StarIcon.jsx";
+import ThankYouIllustration from "./components/ThankYouIllustration.jsx";
 
 const App = () => {
+    const [selectedRating, setSelectedRating] = useState("");
+    const [hasSubmitted, setHasSubmitted] = useState(false);
+
+    const handleRating = (e) => {
+        setSelectedRating(e.target.value.toString());
+    };
+
+    const handleSubmit = () => {
+        if (!selectedRating) return;
+
+        setHasSubmitted((hasSubmitted) => !hasSubmitted);
+    };
+
     return (
         <div className="page">
             <div className="card">
                 <main className="content">
-                    <div className="star-container">
-                        <StarIcon />
-                    </div>
-                    <h2 className="title">How did we do?</h2>
-                    <p className="description">
-                        Please let us know how we did with your support request.
-                        All feedback is appreciated to help us improve our
-                        offering!
-                    </p>
-                    <section className="ratings">
-                        <div className="rating-container">
-                            <span className="one">1</span>
-                        </div>
-                        <div className="rating-container">
-                            <span className="two">2</span>
-                        </div>
-                        <div className="rating-container">
-                            <span className="three">3</span>
-                        </div>
-                        <div className="rating-container">
-                            <span className="four">4</span>
-                        </div>
-                        <div className="rating-container">
-                            <span className="five">5</span>
-                        </div>
-                    </section>
-                    <section className="action">
-                        <button className="submit">Submit</button>
-                    </section>
+                    {!hasSubmitted && (
+                        <>
+                            <div className="star-container">
+                                <StarIcon />
+                            </div>
+                            <h2 className="title">How did we do?</h2>
+                            <p className="description">
+                                Please let us know how we did with your support
+                                request. All feedback is appreciated to help us
+                                improve our offering!
+                            </p>
+                            <section className="ratings">
+                                <button
+                                    className="rating"
+                                    onClick={handleRating}
+                                    value={1}
+                                >
+                                    1
+                                </button>
+                                <button
+                                    className="rating"
+                                    onClick={handleRating}
+                                    value={2}
+                                >
+                                    2
+                                </button>
+                                <button
+                                    className="rating"
+                                    onClick={handleRating}
+                                    value={3}
+                                >
+                                    3
+                                </button>
+                                <button
+                                    className="rating"
+                                    onClick={handleRating}
+                                    value={4}
+                                >
+                                    4
+                                </button>
+                                <button
+                                    className="rating"
+                                    onClick={handleRating}
+                                    value={5}
+                                >
+                                    5
+                                </button>
+                            </section>
+                            <section className="action">
+                                <button
+                                    className="submit"
+                                    onClick={handleSubmit}
+                                >
+                                    Submit
+                                </button>
+                            </section>
+                        </>
+                    )}
+                    {hasSubmitted && (
+                        <section className="thank-you">
+                            <ThankYouIllustration />
+                            <p className="selected-rating">
+                                You selected {selectedRating} out of 5
+                            </p>
+                            <h2>Thank You!</h2>
+                            <p className="gratitude">
+                                We appreciate you taking the time to give a
+                                rating. If you ever need more support,
+                                don&apos;t hesitate to get in touch!
+                            </p>
+                        </section>
+                    )}
                 </main>
             </div>
         </div>
